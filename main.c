157a8aa4a63a10aa0359b6ca305cf7d5f22acf83
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-16 15:28:17 +0900

    001

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int i = 10;
+	char c = 'a';
+	
+	
+	//
+	int* iptr = &i;
+	char* cptr = &c;
+	int* iptr2 = iptr;
+	//
+	
+	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
+	
+	system("PAUSE");
+	return 0;
+}
