ce46e0eef2b29eac1990d101728fb787bc367948
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-16 15:41:40 +0900

    003

--- main.c
+++ main.c
@@ -3,11 +3,19 @@
 
 int main(int argc, char *argv[]) {
 	
+	void swap (int *x, int *y) {
+
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+	}
+	
 	void main(void) {
-	int i = 300; //300으로 변경해서도 해보기
-	int *pi = &i;
-	char *pc = &i;
-	printf("%i, %i, %i\n", i, *pi, *pc);
+	int a = 3;
+	int b = 5;
+	swap(&a, &b);
+	printf("a:%i, b:%i\n", a, b);
 }
 	
 	system("PAUSE");
