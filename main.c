9fab715ff62b312b8073876ccf66bcfba9aa0ae0
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-16 16:03:38 +0900

    005

--- main.c
+++ main.c
@@ -2,26 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
-{
+{	
+	char *pc;
+	int *pi;
+	double *pd;
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
 	
-	int i;
-	int grade[5];
-	int average=0;
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
-	for (i=0;i<5;i++)
-	{
-	printf("input value [%i] = ", i);
-	scanf("%d", &grade[i]);
-	}
-	
-	//
-	for (i=0 ; i<5 ; i++)
-	{
-		printf("grade[%i] = &i\n", i, *(grade+i));
-		average = average + *(grade+i);
-		
-		printf("average = &i\n", average);
-	}
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
 	system("PAUSE");
 	return 0;
